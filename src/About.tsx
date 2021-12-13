7ca6c9fd03edb3606a2b4ddf4464703dcab1f736
Nicole Prindle <nprindle18@gmail.com>
2021-12-13 01:42:08 -0500

    Change headers to bold instead of gray

--- src/About.tsx
+++ src/About.tsx
@@ -8,7 +8,7 @@ export default function About(props: { changePage: (state: Page) => void }) {
 
             <p className="page-text">With the proliferation of the Internet and online services, we can reach more people than ever before. From government services to social media, we not only collect more data on our customers, users, and citizens than ever before, but many services find themselves tasked with the challenge of collecting data from an increasingly diverse and international userbase.</p>
 
-            <p className="page-text">In this environment, our normal assumptions about personal information break down; if we expect a user to always have, for example, a single immutable first and last name or a binary gender, we will end up unintentionally excluding many potential users. The simple design of a form or a database could act as a gate blocking people from important information and services.</p>
+            <p className="page-text">In this environment, our normal assumptions about personal information break down; if we expect a user to always have, for example, a single immutable first and last name or a binary gender, we will end up unintentionally excluding many potential users. The design of a form or a database could act as a gate blocking people from important information and services.</p>
 
             <p className="page-text">As programmers, developers, and designers, this means we have to challenge our usual assumptions, and be conscious of the intersectional impacts of our designs and decisions.</p>
 
